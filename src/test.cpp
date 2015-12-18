4bd51804af474cf887437cb8177c167dbfaa3b18
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-12-19 05:12:31 +0600

    3.3.0

--- src/test.cpp
+++ src/test.cpp
@@ -9,5 +9,5 @@ int main(){
     String f="/media/jahid/softs/iso images/windows/kfljsl.iso";
     String d="/home/jahidul/Downloads/sofs/instant/more-instant";
     String e="/windows/kfljsl.iso";
-    std::cout<<processExtendedPdNameStringRule("/pde/ /pd-3/ /pd/ /pd3-2Ee/ /Pd423423-0--/ /pdfsdf/",f);
+    std::cout<<trimFloatingZero("10000.0020E0098");
     }
