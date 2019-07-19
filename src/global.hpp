2abe05b9ede39f3b8be2e0863db489ff447f83b2
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2019-07-19 16:08:34 +0600

    fix f,d,l modifier for replace string

--- src/global.hpp
+++ src/global.hpp
@@ -303,7 +303,7 @@ String sort_type2;
 /// Project Info
 String project_name="rnm";
 String executable_name="rnm";
-String version="4.0.5";
+String version="4.0.6";
 String author_name="Md Jahidul Hamid";
 String author_email="jahidulhamid@yahoo.com";
 String bug_report_url="https://github.com/neurobin/"+project_name+"/issues";
